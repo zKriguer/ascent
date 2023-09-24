c6f25d98e0ab496d6f66157ec4d87bf34819070a
¨zKriguer¨ <kriguerfreitas2020@gmail.com>
2023-09-23 22:17:42 -0300

    Fix: under development tag

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Nav = (props: Props) => {
   return (
     <div className="w-full bg-zinc-900 h-16 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 absolute justify-center items-center">
-      <p>Nav</p>
+      <p className="text-orange-600">Under development</p>
     </div>
   );
 };
