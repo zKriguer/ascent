8ddc88e798b997c913a4566647d380970feb406b
¨zKriguer¨ <kriguerfreitas2020@gmail.com>
2023-10-04 12:24:56 -0300

    feat: prices card update

--- src/components/PlansSection.tsx
+++ src/components/PlansSection.tsx
@@ -25,42 +25,13 @@ const PlansSection = (props: Props) => {
 
   return (
     <div className="flex w-full items-center justify-center gap-12 h-full">
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        initialSlide={1}
-        slidesPerView={"auto"}
-        navigation={true}
-        autoplay={{
-          delay: 1000,
-          disableOnInteraction: true,
-          pauseOnMouseEnter: true,
-        }}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
-        className={twMerge("mySwiper", "flex sm:hidden")}
-      >
+      <div className="gap-8 flex-wrap items-center justify-center flex">
         {plans.map((item: Plan) => (
-          <SwiperSlide
-            className="flex items-center justify-center"
-            key={item.id}
-          >
-            <PriceCard plan={item} checkboxKey="slider" />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      <div className="gap-8 flex-wrap items-center justify-center hidden sm:flex">
-        {plans.map((item: Plan) => (
-          <PriceCard plan={item} key={item.id} checkboxKey="flexbox" />
+          <PriceCard
+            plan={item}
+            key={item.id + "flexbox"}
+            checkboxKey="flexbox"
+          />
         ))}
       </div>
     </div>
