4e91a915c16ce9162806d640496efc7a548a39c5
Fellipe Utaka <fellipeutaka@gmail.com>
2023-10-06 16:24:52 -0300

    chore: remove console on production mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,10 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  vite: {
+    esbuild: {
+      drop:
+        process.env.NODE_ENV === "production" ? ["console", "debugger"] : [],
+    },
+  },
 });
